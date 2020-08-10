e052ccdc65f78c74d8cbe6a313bfebdf1c08de43
Olutoke Damola <olutokeezekiel18@gmail.com>
2020-08-10 07:38:09 +0100

    Made changes

--- request.js
+++ request.js
@@ -6,7 +6,6 @@ const weatherCards = document.querySelector('.weather-cards');
 
 
 updateWeatherSearch = (city) => {
-    console.log(city);
     cityName.textContent = city.name;
     description.innerHTML = `
     <div class="description-view">
@@ -19,30 +18,23 @@ updateWeatherSearch = (city) => {
             <div id="Temperature">
                 <span class="iconify" id="iconify" data-icon="fa-solid:temperature-high" data-inline="false"  style="color:red";></span><br>
                 <p id="weather-text">Temperature</p>
-                <p style="font-weight:bold;">${city.main.temp}<span>&#8451</span></p>
+                <p id="weather-value">${city.main.temp}<span>&#8451</span></p>
             </div>
             <div id="Humidity">
                 <span class="iconify" id="iconify" data-icon="mdi:water-percent" data-inline="false" style="color:#5a7ae4";></span><br>
                 <p id="weather-text">Humidity</p>
-                <p style="font-weight:bold;"><span>${city.main.humidity}</span>%</p>
+                <p id="weather-value"><span>${city.main.humidity}</span>%</p>
             </div>
             <div id="Wind">
                 <span class="iconify" id="iconify" data-icon="uil:wind" data-inline="false" style="color:#81C2EB;"></span><br>
                 <p id="weather-text">Wind</p>
-                <p style="font-weight:bold;">
-                <p style="font-weight:bold;">
-                    <ul>
-                        <li><h5 id="speed">${city.wind.speed}</h5><span>mph</span></li>
-                        <li>/</li>
-                        <li><h5 id="deg">${city.wind.deg}</h5><span style="font-weight: bold; font-size:24px;">&deg</span></li>
-                    </ul>
-                </p>
-                </p>
+                <p id="weather-value">
+                <p id="weather-value"><span>${city.wind.speed}</span>mph</p>
             </div>
             <div id="Visibility">
                 <span class="iconify" id="iconify" data-icon="tabler:mist" data-inline="false" style="color: #FCC418";></span><br>
                 <p id="weather-text">Visibilty</p>
-                <p style="font-weight:bold;">${city.visibility}<span>km</span></p>
+                <p id="weather-value">${city.visibility}<span>km</span></p>
             </div>
         </div>
     `
@@ -52,7 +44,7 @@ searchBox.addEventListener('submit', e => {
     e.preventDefault();  // This event event method prevents the form from refreshing anytime a request is made
     const citySearch = cityValue.value.trim();
     console.log(citySearch);
-    searchBox.reset();
+    // searchBox.reset(); This clears the inputed text from the searchBox once the output is queried 
 
     requestCity(citySearch)
         .then((data) =>{
