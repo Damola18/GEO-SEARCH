fb29cecd722bbcd9474e561151eea281deb06601
Olutoke Damola <olutokeezekiel18@gmail.com>
2020-11-25 23:53:29 +0100

    Update

--- request.js
+++ request.js
@@ -2,6 +2,7 @@ const requestCity = async (city) =>{
     const api_key = '14e09d5264977022ce3545c1661fccf0';
     const apiURL = 'https://api.openweathermap.org/data/2.5/weather';
     const query = `?q=${city}&appid=${api_key}`;
+    
     // Alternate concatenation method for declaring the query variable
     // const query = '?q='+city+'&appid'+key;
 
